b276d84f193f8d3655189c61c59e87891081ba58
BBWORKSTATION\bjoer <bjoern.bajor@gmail.com>
2025-11-13 15:26:08 +0100

    fix: AnalyticsRepository zu @EnableJpaRepositories hinzugefügt

--- src/main/java/de/bbajor/pvs/Application.java
+++ src/main/java/de/bbajor/pvs/Application.java
@@ -22,7 +22,8 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
         "de.bbajor.pvs.appointment.repository",
         "de.bbajor.pvs.security.domain",
         "de.bbajor.pvs.security.email.repository",
-        "de.bbajor.pvs.ai.repository"
+        "de.bbajor.pvs.ai.repository",
+        "de.bbajor.pvs.analytics.repository"
 })
 @EntityScan(basePackages = {
         "de.bbajor.pvs.patient.model",
